84e5498e15ae23eb8aa6a04f75d809b8a7170660
github username <github email address>
2019-08-27 11:59:56 -0400

    can get token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
   def encode_token(payload)
-    # don't forget to hide your secret in an environment variable
+    # should store secret in env variable
     JWT.encode(payload, 'my_s3cr3t')
   end
 
   def auth_header
+    # { Authorization: 'Bearer <token>' }
     request.headers['Authorization']
   end
 
   def decoded_token
     if auth_header
       token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
       begin
         JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
       rescue JWT::DecodeError
@@ -22,8 +25,6 @@ class ApplicationController < ActionController::API
 
   def current_user
     if decoded_token
-      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
-      # or nil if we can't decode the token
       user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
     end
@@ -31,6 +32,10 @@ class ApplicationController < ActionController::API
 
   def logged_in?
     !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   # def index
   #   @users = User.all
@@ -8,13 +9,15 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: @user, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :password, :bio, :avatar)
   end
