07049a2cfed032a45ddafbac8d9f256cf81ed277
github username <github email address>
2019-08-27 11:53:44 -0400

    auth progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: @user, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :bio, :avatar)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :games_problems
   resources :games
   resources :problems
+
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
