ee9c011d24d2b715dea8c1247190daebfeb0648b
github username <randyherasme6@gmail.com>
2019-12-09 12:54:09 -0500

    clean up controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,5 @@
 class GamesController < ApplicationController
 
-  before_action :find_game, only: [:update, :show, :destroy]
-
   def index
     @games = Game.all
     render json: @games
@@ -22,17 +20,10 @@ class GamesController < ApplicationController
     render json: @game
   end
 
- 
-
-
-
   private
 
     def game_params
     params.require(:game).permit(:user_id, :score, :time_remaining, :problems, :game_type, :number_sentences)
     end
 
-    def find_game
-        @game = Game.find(params[:id])
-    end
 end
